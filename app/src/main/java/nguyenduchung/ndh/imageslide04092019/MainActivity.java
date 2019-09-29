1d9ea5f07481c22b4da34d54a9993c0d28a7064f
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-30 00:41:00 +0700

    A

--- app/src/main/java/nguyenduchung/ndh/imageslide04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/imageslide04092019/MainActivity.java
@@ -3,12 +3,36 @@ package nguyenduchung.ndh.imageslide04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView mimg;
+    Button mbtnSlide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+        //countdowtimer
+        CountDownTimer countDownTimer=new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        countDownTimer.start();
+    }
+
+    private void initview() {
+        mbtnSlide=findViewById(R.id.buttonShow);
+        mimg=findViewById(R.id.imageview);
     }
 }
