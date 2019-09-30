9b310e942196940e67754c4fe390b6923f429f27
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-30 21:24:20 +0700

    A

--- app/src/main/java/nguyenduchung/ndh/imageslide04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/imageslide04092019/MainActivity.java
@@ -5,30 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView mimg;
     Button mbtnSlide;
+    int[] marrayannimal={R.drawable.iccat,R.drawable.icmonster,R.drawable.icontrue};
+    int index=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initview();
+        event();
+
+
         //countdowtimer
-        CountDownTimer countDownTimer=new CountDownTimer(10000,1000) {
+
+    }
+    private void event(){
+        mbtnSlide.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTick(long l) {
+            public void onClick(View v) {
+                final CountDownTimer countDownTimer=new CountDownTimer(1000,1000) {
+                    @Override
+                    public void onTick(long l) {
+                        if(index == marrayannimal.length){
+                            index=0;
+                        }
+                        mimg.setImageResource(marrayannimal[index++]);
 
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        this.start();
+                    }
+                };
+                countDownTimer.start();
             }
-
-            @Override
-            public void onFinish() {
-
-            }
-        };
-        countDownTimer.start();
+        });
     }
 
     private void initview() {
